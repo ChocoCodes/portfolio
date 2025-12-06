2dac9076b45ab0e8db21cfdb5e46636440d13fb4
ChocoCodes <chocojhong999@gmail.com>
2025-12-07 00:28:23 +0800

    feat: available icon on hero

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -9,10 +9,16 @@ export const Hero = () => {
        <div className="flex flex-col w-full">
             <div className="flex flex-col justify-start w-4/5 md:w-3/5 border gap-2 md:gap-4 border-accent rounded-lg mx-auto p-4 md:py-6 md:px-8">
                 <div className="flex justify-between items-center">
-                    <p className='font-sora font-medium text-sm md:text-3xl text-default'>John Roland Octavio</p>
+                    <div className="flex gap-2 md:gap-4 items-center">
+                        <p className='font-sora font-medium text-[12px] md:text-3xl text-default'>John Roland Octavio</p>
+                        <div className="flex items-center gap-1 md:gap-2 text-[6px] md:text-sm text-secondary font-manrope">
+                            <div className="w-1 md:w-2 h-1 md:h-2 rounded-full bg-green"></div>
+                            <p>Available</p>
+                        </div>
+                    </div>
                     <SocialLinks />
                 </div>
-                <p className="inline-flex gap-1 text-[10px] lg:text-lg items-center font-manrope text-secondary -mt-2 md:-mt-4"> <GrLocationPin /> Bacolod City, Philippines</p>
+                <p className="inline-flex gap-1 text-[8px] lg:text-lg items-center font-manrope text-secondary -mt-2 md:-mt-4"> <GrLocationPin /> Bacolod City, Philippines</p>
                 <p className="w-full text-[10px] lg:text-lg text-default">
                     I specialize in building clean, practical, and effective solutions for 3 years. 
                     Currently serving as the <span className="font-semibold">Chief Technology Officer</span> and a former <span className="font-semibold">Web Development Lead</span> of  
