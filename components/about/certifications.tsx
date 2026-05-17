87a4be494b21bb5f1cc24b3620544d31d761ee61
ChocoCodes <chocojhong999@gmail.com>
2026-05-17 23:12:18 +0800

    fix: certificate page layout consistency

--- components/about/certifications.tsx
+++ components/about/certifications.tsx
@@ -7,12 +7,12 @@ import { Carousel } from '../carousel';
 
 export const Certifications = () => {
     return (
-        <div className="flex flex-col gap-6 w-4/5 lg:w-3/5 my-auto">
+        <div className="flex flex-col gap-6 w-full my-auto">
             <StaggerWrapper delay={ 0.1 } className="text-center font-sora text-default">
                 <p className="text-xl md:text-4xl text-default font-sora font-semibold">Certifications</p>
                 <p className='mt-2 text-xs md:text-xl text-secondary font-manrope font-medium'>Certifications from platforms, programs, and trainings I have completed.</p>
             </StaggerWrapper>
-            <StaggerWrapper delay={ 0.3 } className="w-full mx-auto">
+            <StaggerWrapper delay={ 0.3 } className="w-full max-w-4/5 mx-auto">
                 <Carousel items={ CERTS } renderItem={ cert => <CertificateCard {...cert} /> }/>
             </StaggerWrapper>
         </div>
